338542785fe63fa438ed3ee4afbcbc934e53c62b
Ky Huynh <ky.huynh@axonactive.com>
2020-10-01 12:56:41 +0700

    remove fixed string "JabRef" when generating csv file's name

--- src/control/OSSParser.java
+++ src/control/OSSParser.java
@@ -124,7 +124,7 @@ public class OSSParser {
 		
 		log += fd.insertProject(project, dirTrab, format);
 		try {
-			os = new FileOutputStream(dirTrab + "JabRefImports.csv");
+			os = new FileOutputStream(dirTrab + " Imports.csv");
 			osw = new OutputStreamWriter(os);
 			bw = new BufferedWriter(osw);
 	    	bw.write("File,Word,Complement \n");
